68d35608e4fdf92d87d5a936c50e788014c95bbe
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-01-26 13:40:53 -0300

    Usuario unico

---
+++ exercicios/desafioUnicoUsuario.js
@@ -0,0 +1,9 @@
+const env = require('../.env');
+const Telegraf = require('telegraf');
+const bot = new Telegraf(env.token);
+
+bot.start(ctx => {
+    ctx.update.message.from.id === 1255407233 ? ctx.reply('Ao seu dispor, mestre!') : ctx.reply('Sinto muito, mas eu só falo com o meu mestre');
+});
+
+bot.startPolling();
