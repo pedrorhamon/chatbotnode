be3adb5b5988cf475022311eb1e0d5f7aefbd8bc
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-01-31 17:54:44 -0300

    varias maneiras de reply

---
+++ exercicios/tipoDeReposta.js
@@ -0,0 +1,11 @@
+const env = require('../.env');
+const Telegraf = require('telegraf');
+const bot = new Telegraf(env.token);
+
+bot.start(async ctx => {
+    await ctx.reply(`Seja bem vindo, ${ctx.update.message.from.first_name}`)
+    await ctx.replyWithHTML();
+    await ctx.replyWithMarkdown();
+})
+
+bot.startPolling();
